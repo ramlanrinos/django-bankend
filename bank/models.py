72d9f792dfca77e8f3534a7eb298058c4db9083e
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-01 09:33:33 +0530

    Account and Transaction models created

--- bank/models.py
+++ bank/models.py
@@ -9,3 +9,31 @@ class Customer(models.Model):
  
     def __str__(self): 
         return self.name
+    
+class Account(models.Model): 
+    ACCOUNT_TYPES = [ 
+        ('SAV', 'Savings'), 
+        ('CUR', 'Current'), 
+    ] 
+ 
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE) 
+    account_type = models.CharField(max_length=3, choices=ACCOUNT_TYPES) 
+    balance = models.DecimalField(max_digits=12, decimal_places=2) 
+    created_at = models.DateField(auto_now_add=True) 
+ 
+    def __str__(self): 
+        return f"{self.customer.name} - {self.account_type}"
+
+class Transaction(models.Model): 
+    account = models.ForeignKey(Account, on_delete=models.CASCADE) 
+    TRANSACTION_TYPES = [ 
+        ('D', 'Deposit'), 
+        ('W', 'Withdrawal'), 
+    ] 
+    transaction_type = models.CharField(max_length=1, 
+choices=TRANSACTION_TYPES) 
+    amount = models.DecimalField(max_digits=10, decimal_places=2) 
+    timestamp = models.DateTimeField(auto_now_add=True) 
+ 
+    def __str__(self): 
+        return f"{self.get_transaction_type_display()} of {self.amount} on {self.timestamp}" 
