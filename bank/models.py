06434668098643187e6576ed01c4733760565f21
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-01 09:32:19 +0530

    Customer model created

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model): 
+    name = models.CharField(max_length=100) 
+    dob = models.DateField() 
+    email = models.EmailField(unique=True) 
+    address = models.TextField() 
+ 
+    def __str__(self): 
+        return self.name
