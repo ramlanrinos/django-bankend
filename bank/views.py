7df59923fd539c92797518e90d181ef8c5e1ad52
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-01 09:39:09 +0530

    customer_list view created

--- bank/views.py
+++ bank/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse 
+from .models import Customer
 
 # Create your views here.
+def customer_list(request): 
+    data = list(Customer.objects.values()) 
+    return JsonResponse(data, safe=False) 
